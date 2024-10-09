1960593c34f8bab5fca0cc50e782ecbbf1f027e9
nicolasdri92 <47433958+nicolasdri92@users.noreply.github.com>
2024-10-08 22:18:39 -0300

    Delete app directory

--- app/layout/auth/auth.page.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: '.auth',
-  templateUrl: './auth.page.html',
-})
-export class AuthPage {}
--- app/layout/main/main.page.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: '.main',
-  templateUrl: './main.page.html',
-})
-export class MainPage {}
